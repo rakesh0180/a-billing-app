a02135013783e39bd4cc72707b36f0899e0132ec
Rakesh.M <rakeshraki0180@gmail.com>
2021-09-13 21:24:56 +0530

    added 404 page different message for login user and not login users

--- src/component/user/NotFound.js
+++ src/component/user/NotFound.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Link, useHistory, useParams } from "react-router-dom";
 import error404 from "../../assets/img/error/404-error.png";
 
@@ -6,6 +7,8 @@ function NotFound(props) {
   const params = useParams();
   const history = useHistory();
   console.log(params);
+
+  const userLoginStatus = useSelector((state) => state.userLoginStatus);
   return (
     <>
       <div className="error-404 d-flex align-items-center justify-content-center">
@@ -20,28 +23,53 @@ function NotFound(props) {
                     <span className="text-success">4</span>
                   </h1>
                   <h2 className="font-weight-bold display-4">Lost in Space</h2>
-                  <p>
-                    You have reached the edge of the universe.
-                    <br />
-                    The page you requested could not be found
-                    <strong> {params.id}</strong>.
-                    <br />
-                    Dont'worry and return to the previous page.
-                  </p>
-                  <div className="mt-5">
-                    <Link
-                      to="/dashboard"
-                      className="btn btn-lg btn-primary px-md-5 radius-30"
-                    >
-                      Go DashBoard
-                    </Link>
-                    <button
-                      className="btn btn-lg btn-outline-dark ml-3 px-md-5 radius-30"
-                      onClick={history.goBack}
-                    >
-                      Back
-                    </button>
-                  </div>
+                  {userLoginStatus ? (
+                    <>
+                      <p>
+                        You have reached the edge of the universe.
+                        <br />
+                        The page you requested could not be found
+                        <strong> {params.id}</strong>.
+                        <br />
+                        Dont'worry and return to the previous page.
+                      </p>
+                      <div className="mt-5">
+                        <Link
+                          to="/dashboard"
+                          className="btn btn-lg btn-primary px-md-5 radius-30"
+                        >
+                          Go DashBoard
+                        </Link>
+                        <button
+                          className="btn btn-lg btn-outline-dark ml-3 px-md-5 radius-30"
+                          onClick={history.goBack}
+                        >
+                          Back
+                        </button>
+                      </div>
+                    </>
+                  ) : (
+                    <>
+                      <p>
+                        You have reached the edge of the universe.
+                        <br />
+                        The page you requested could not be found
+                        <strong> {params.id}</strong>.
+                        <br />
+                        Dont'worry first <strong>signup</strong> or{" "}
+                        <strong>Login</strong>.
+                        <br />
+                      </p>
+                      <div className="mt-5">
+                        <Link
+                          to="/"
+                          className="btn btn-lg btn-primary px-md-5 radius-30"
+                        >
+                          Go to Login page
+                        </Link>
+                      </div>
+                    </>
+                  )}
                 </div>
               </div>
               <div className="col-lg-6">
