f114fce4a258135d82b60a0f76e3cdf19b071168
Rakesh.M <rakeshraki0180@gmail.com>
2021-09-17 18:51:18 +0530

    added webpackChunkName

--- src/App.js
+++ src/App.js
@@ -1,30 +1,52 @@
-import React, { useEffect } from "react";
+import React, { lazy, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import { setUserLoginStatus } from "./action/userLoginStatusAction";
 import "./App.css";
-import NotFound from "./component/user/NotFound";
-const BillSubContainer = React.lazy(() =>
-  import("./component/bill/BillSubContainer")
+const NotFound = lazy(() =>
+  import(/* webpackChunkName:"NotFound" */ "./component/user/NotFound")
 );
-const CustomerContainer = React.lazy(() =>
-  import("./component/customer/CustomerContainer")
+const BillSubContainer = lazy(() =>
+  import(/* webpackChunkName:"BillSub" */ "./component/bill/BillSubContainer")
 );
-const DashboardContainer = React.lazy(() =>
-  import("./component/dashboard/DashboardContainer")
+const CustomerContainer = lazy(() =>
+  import(
+    /* webpackChunkName:"Customer" */ "./component/customer/CustomerContainer"
+  )
 );
-const ProductContainer = React.lazy(() =>
-  import("./component/product/ProductContainer")
+const DashboardContainer = lazy(() =>
+  import(
+    /* webpackChunkName:"Dashboard" */ "./component/dashboard/DashboardContainer"
+  )
 );
-const Home = React.lazy(() => import("./component/user/Home"));
-const Login = React.lazy(() => import("./component/user/Login"));
-const Navbar = React.lazy(() => import("./component/user/Navbar"));
-const PrivateRoute = React.lazy(() => import("./component/user/PrivateRoute"));
-const PublicRoute = React.lazy(() => import("./component/user/PublicRoute"));
-const Signup = React.lazy(() => import("./component/user/Signup"));
-const UserDetails = React.lazy(() => import("./component/user/UserDetails"));
-const BillContainer = React.lazy(() =>
-  import("./component/bill/BillContainer")
+const ProductContainer = lazy(() =>
+  import(
+    /* webpackChunkName:"Product" */ "./component/product/ProductContainer"
+  )
+);
+const Home = lazy(() =>
+  import(/* webpackChunkName:"Home" */ "./component/user/Home")
+);
+const Login = lazy(() =>
+  import(/* webpackChunkName:"Login" */ "./component/user/Login")
+);
+const Navbar = lazy(() =>
+  import(/* webpackChunkName:"Navbar" */ "./component/user/Navbar")
+);
+const PrivateRoute = lazy(() =>
+  import(/* webpackChunkName:"PrivateRoute" */ "./component/user/PrivateRoute")
+);
+const PublicRoute = lazy(() =>
+  import(/* webpackChunkName:"PublicRoute" */ "./component/user/PublicRoute")
+);
+const Signup = lazy(() =>
+  import(/* webpackChunkName:"Signup" */ "./component/user/Signup")
+);
+const UserDetails = lazy(() =>
+  import(/* webpackChunkName:"User" */ "./component/user/UserDetails")
+);
+const BillContainer = lazy(() =>
+  import(/* webpackChunkName:"NotFound" */ "./component/bill/BillContainer")
 );
 
 function App() {
